918cd48cd329cd029351d56a3570ffed23c30f5e
Sandra Siegel <11627788+sandrasie@users.noreply.github.com>
2020-04-01 21:37:13 +0200

    Layout and styles for start page

--- quiz-master-client/src/App.tsx
+++ quiz-master-client/src/App.tsx
@@ -36,29 +36,50 @@ function App() {
 
     return (
         <div className="App">
-            <header className="App-header">
-                { quiz.id
-                  ?
-                    <QuizMaster quiz={quiz}></QuizMaster>
-                  :
-                    <div>
-                        <p>
-                            <span><label>Quiz name</label></span><span><input type="text" onChange={(ev) => setQuizName(ev.target.value)} /></span>
-                            <button onClick={startQuiz}>Start quiz</button>
-                        </p>
-                        <div>
-                            <h3>Join quiz</h3>
-                            <div>
-                                <span><label>Quiz ID</label></span><span><input type="text" onChange={(ev) => setQuizId(ev.target.value)} /></span>
+            <header className="level App-header">
+                <div className="icon App-logo">
+                    <i className="far fa-question-circle"></i>
+                </div> 
+                QuizMaster
+            </header>
+            
+            { quiz.id
+                ?
+                <QuizMaster quiz={quiz}></QuizMaster>
+                :
+                <div className="container App-content">
+                   
+                    <div className="box">
+                        <h5 className="title is-5">Create a quiz</h5>
+                        <div className="field has-addons">
+                            <div className="control">
+                                <input className="input" type="text" placeholder="Quiz Name"  onChange={(ev) => setQuizName(ev.target.value)} />
                             </div>
-                            <div>
-                                <button onClick={joinQuiz}>GO!</button>
+                            <div className="control">
+                                <button className="button is-info" onClick={startQuiz}>
+                                Start!
+                                </button>
                             </div>
                         </div>
                     </div>
-                }
+
+                    <div className="box">
+                        <h5 className="title is-5">Join a quiz</h5>
+                        <div className="field has-addons">
+                            <div className="control">
+                                <input className="input" type="text" placeholder="Quiz ID" onChange={(ev) => setQuizId(ev.target.value)}/>
+                            </div>
+                            <div className="control">
+                                <button className="button is-info" onClick={joinQuiz}>
+                                Join!
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
                 
-            </header>
+            
         </div>
     );
 }
