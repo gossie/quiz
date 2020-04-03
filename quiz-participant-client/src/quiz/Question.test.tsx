14c8769dcb6e8f7ecd958b9c8e04e5936cabb5db
André Schreck <andre.schreck@gmail.com>
2020-04-03 23:46:45 +0200

    issue #5: added unit test for participant question component

---
+++ quiz-participant-client/src/quiz/Question.test.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Quiz from './quiz';
+import Question from './Question';
+
+test('has no questions', () => {
+    const quiz: Quiz = {
+        id: 5,
+        name: "Awesome Quiz",
+        participants: [],
+        questions: [],
+        links: []
+    }
+    const { getByTestId } = render(<Question quiz={quiz} />);
+    
+    expect(() => getByTestId('current-question')).toThrowError('Unable to find an element by: [data-testid="current-question"]');
+});
+
+test('has no pending question', () => {
+    const quiz: Quiz = {
+        id: 5,
+        name: "Awesome Quiz",
+        participants: [],
+        questions: [
+            {
+                question: "What is happening?",
+                pending: false,
+                links: []
+            }
+        ],
+        links: []
+    }
+    const { getByTestId } = render(<Question quiz={quiz} />);
+    
+    expect(() => getByTestId('current-question')).toThrowError('Unable to find an element by: [data-testid="current-question"]');
+});
+
+test('has pending question', () => {
+    const quiz: Quiz = {
+        id: 5,
+        name: "Awesome Quiz",
+        participants: [],
+        questions: [
+            {
+                question: 'What is happening?',
+                pending: true,
+                links: []
+            }
+        ],
+        links: []
+    }
+    const { getByTestId } = render(<Question quiz={quiz} />);
+
+    const currentQuestion = getByTestId('current-question');
+    
+    expect(currentQuestion.textContent).toEqual('What is happening?');
+});
