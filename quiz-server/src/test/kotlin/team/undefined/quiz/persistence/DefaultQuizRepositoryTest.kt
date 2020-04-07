2981c1adab18144e37aee1491cd45e7c35035a37
André Schreck <andre.schreck@gmail.com>
2020-04-07 17:25:44 +0200

    #4: persist if a question was already played

--- quiz-server/src/test/kotlin/team/undefined/quiz/persistence/DefaultQuizRepositoryTest.kt
+++ quiz-server/src/test/kotlin/team/undefined/quiz/persistence/DefaultQuizRepositoryTest.kt
@@ -1,6 +1,7 @@
 package team.undefined.quiz.persistence
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest
@@ -11,6 +12,7 @@ import team.undefined.quiz.core.Question
 import team.undefined.quiz.core.Quiz
 import team.undefined.quiz.core.QuizService
 import team.undefined.quiz.web.ReactiveWebSocketHandler
+import java.util.concurrent.atomic.AtomicLong
 
 @DataR2dbcTest
 @Import(DefaultQuizRepository::class, QuizService::class, ReactiveWebSocketHandler::class, ObjectMapper::class)
@@ -21,24 +23,49 @@ internal class DefaultQuizRepositoryTest {
 
     @Test
     fun shouldCreateAndDetermineChangeAndSaveQuiz() {
+        val quizId = AtomicLong(-1)
+
         StepVerifier.create(defaultQuizRepository.createQuiz(Quiz(name = "Quiz")))
-                .expectNext(Quiz(1, "Quiz"))
+                .consumeNextWith {
+                    quizId.set(it.id!!)
+                    assertThat(it).isEqualTo(Quiz(it.id, "Quiz"))
+                }
                 .verifyComplete()
 
-        StepVerifier.create(defaultQuizRepository.determineQuiz(1))
-                .expectNext(Quiz(1, "Quiz"))
+        StepVerifier.create(defaultQuizRepository.determineQuiz(quizId.get()))
+                .expectNext(Quiz(quizId.get(), "Quiz"))
                 .verifyComplete()
 
-        StepVerifier.create(defaultQuizRepository.saveQuiz(Quiz(1, "Quiz", listOf(Participant(name = "Sandra"), Participant(name = "Allli"), Participant(name = "Erik", turn = true)), listOf(Question(question = "Wofür steht eigentlich a.D.?")))))
-                .expectNext(Quiz(1, "Quiz", listOf(Participant(1, "Sandra"), Participant(2, "Allli"), Participant(3, "Erik", true)), listOf(Question(1, "Wofür steht eigentlich a.D.?"))))
+        val participant1Id = AtomicLong(-1)
+        val participant2Id = AtomicLong(-1)
+        val participant3Id = AtomicLong(-1)
+        val questionId = AtomicLong(-1)
+
+        StepVerifier.create(defaultQuizRepository.saveQuiz(Quiz(quizId.get(), "Quiz", listOf(Participant(name = "Sandra"), Participant(name = "Allli"), Participant(name = "Erik", turn = true)), listOf(Question(question = "Wofür steht eigentlich a.D.?")))))
+                .consumeNextWith {
+                    participant1Id.set(it.participants[0].id!!)
+                    participant2Id.set(it.participants[1].id!!)
+                    participant3Id.set(it.participants[2].id!!)
+                    questionId.set(it.questions[0].id!!)
+
+                    assertThat(it).isEqualTo(Quiz(quizId.get(), "Quiz", listOf(Participant(participant1Id.get(), "Sandra"), Participant(participant2Id.get(), "Allli"), Participant(participant3Id.get(), "Erik", true)), listOf(Question(questionId.get(), "Wofür steht eigentlich a.D.?"))))
+                }
                 .verifyComplete()
 
-        StepVerifier.create(defaultQuizRepository.saveQuiz(Quiz(1, "Quiz", listOf(Participant(1, "Sandra"), Participant(2, "Allli"), Participant(3, "Erik", points = 1)), listOf(Question(1, "Wofür steht eigentlich a.D.?")))))
-                .expectNext(Quiz(1, "Quiz", listOf(Participant(1, "Sandra"), Participant(2, "Allli"), Participant(3, "Erik", points = 1)), listOf(Question(1, "Wofür steht eigentlich a.D.?"))))
+        StepVerifier.create(defaultQuizRepository.saveQuiz(Quiz(quizId.get(), "Quiz", listOf(Participant(participant1Id.get(), "Sandra"), Participant(participant2Id.get(), "Allli"), Participant(participant3Id.get(), "Erik", points = 1)), listOf(Question(questionId.get(), "Wofür steht eigentlich a.D.?")))))
+                .expectNext(Quiz(quizId.get(), "Quiz", listOf(Participant(participant1Id.get(), "Sandra"), Participant(participant2Id.get(), "Allli"), Participant(participant3Id.get(), "Erik", points = 1)), listOf(Question(questionId.get(), "Wofür steht eigentlich a.D.?"))))
                 .verifyComplete()
 
-        StepVerifier.create(defaultQuizRepository.saveQuiz(Quiz(1, "Quiz", listOf(Participant(1, "Sandra"), Participant(2, "Allli"), Participant(3, "Erik", points = 1)), listOf(Question(1, "Wofür steht eigentlich a.D.?", imagePath = "pathToImage")))))
-                .expectNext(Quiz(1, "Quiz", listOf(Participant(1, "Sandra"), Participant(2, "Allli"), Participant(3, "Erik", points = 1)), listOf(Question(1, "Wofür steht eigentlich a.D.?", imagePath = "pathToImage"))))
+        StepVerifier.create(defaultQuizRepository.saveQuiz(Quiz(quizId.get(), "Quiz", listOf(Participant(participant1Id.get(), "Sandra"), Participant(participant2Id.get(), "Allli"), Participant(participant3Id.get(), "Erik", points = 1)), listOf(Question(questionId.get(), "Wofür steht eigentlich a.D.?", imagePath = "pathToImage")))))
+                .expectNext(Quiz(quizId.get(), "Quiz", listOf(Participant(participant1Id.get(), "Sandra"), Participant(participant2Id.get(), "Allli"), Participant(participant3Id.get(), "Erik", points = 1)), listOf(Question(questionId.get(), "Wofür steht eigentlich a.D.?", imagePath = "pathToImage"))))
+                .verifyComplete()
+
+        StepVerifier.create(defaultQuizRepository.saveQuiz(Quiz(quizId.get(), "Quiz", listOf(Participant(participant1Id.get(), "Sandra"), Participant(participant2Id.get(), "Allli"), Participant(participant3Id.get(), "Erik", points = 1)), listOf(Question(questionId.get(), "Wofür steht eigentlich a.D.?", imagePath = "pathToImage", pending = true)))))
+                .expectNext(Quiz(quizId.get(), "Quiz", listOf(Participant(participant1Id.get(), "Sandra"), Participant(participant2Id.get(), "Allli"), Participant(participant3Id.get(), "Erik", points = 1)), listOf(Question(questionId.get(), "Wofür steht eigentlich a.D.?", imagePath = "pathToImage", pending = true))))
+                .verifyComplete()
+
+        StepVerifier.create(defaultQuizRepository.saveQuiz(Quiz(quizId.get(), "Quiz", listOf(Participant(participant1Id.get(), "Sandra"), Participant(participant2Id.get(), "Allli"), Participant(participant3Id.get(), "Erik", points = 1)), listOf(Question(questionId.get(), "Wofür steht eigentlich a.D.?", imagePath = "pathToImage", alreadyPlayed = true)))))
+                .expectNext(Quiz(quizId.get(), "Quiz", listOf(Participant(participant1Id.get(), "Sandra"), Participant(participant2Id.get(), "Allli"), Participant(participant3Id.get(), "Erik", points = 1)), listOf(Question(questionId.get(), "Wofür steht eigentlich a.D.?", imagePath = "pathToImage", alreadyPlayed = true))))
                 .verifyComplete()
     }
 
