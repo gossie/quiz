1593355a4b89f3ee126c3507a05341081f63d112
André Schreck <andre.schreck@gmail.com>
2020-04-08 23:43:33 +0200

    #2: do not reinitialize

--- quiz-server/src/main/kotlin/team/undefined/quiz/QuizApplication.kt
+++ quiz-server/src/main/kotlin/team/undefined/quiz/QuizApplication.kt
@@ -16,9 +16,9 @@ import team.undefined.quiz.web.ReactiveWebSocketHandler
 
 @SpringBootApplication
 class QuizApplication {
-
+/*
 	@Bean
-	fun initializer(connectionFactory: ConnectionFactory): ConnectionFactoryInitializer? {
+	fun initializer(connectionFactory: ConnectionFactory): ConnectionFactoryInitializer {
 		val initializer = ConnectionFactoryInitializer()
 		initializer.setConnectionFactory(connectionFactory)
 		val populator = CompositeDatabasePopulator()
@@ -26,7 +26,7 @@ class QuizApplication {
 		initializer.setDatabasePopulator(populator)
 		return initializer
 	}
-
+*/
 	@Bean
 	fun webSocketHandlerMapping(webSocketHandler: ReactiveWebSocketHandler): HandlerMapping {
 		return SimpleUrlHandlerMapping(mapOf(Pair("/event-emitter/{quizId}", webSocketHandler)), 1)
