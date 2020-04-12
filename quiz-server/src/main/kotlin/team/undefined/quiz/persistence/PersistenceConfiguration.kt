e7ccd0be0ac64e5ec1f787b1ac5f7c7e5af2f47c
André Schreck <andre.schreck@gmail.com>
2020-04-12 10:05:30 +0200

    fix database creation

---
+++ quiz-server/src/main/kotlin/team/undefined/quiz/persistence/PersistenceConfiguration.kt
@@ -0,0 +1,8 @@
+package team.undefined.quiz.persistence
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
+
+@Configuration
+@EnableR2dbcRepositories
+class PersistenceConfiguration
