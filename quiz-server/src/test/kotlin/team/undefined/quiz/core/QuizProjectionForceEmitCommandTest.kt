e0516445997c4c0f445f7662d4f45638190c2361
André Schreck <andre.schreck@gmail.com>
2020-04-28 11:25:53 +0200

    #39: added unit test

---
+++ quiz-server/src/test/kotlin/team/undefined/quiz/core/QuizProjectionForceEmitCommandTest.kt
@@ -0,0 +1,36 @@
+package team.undefined.quiz.core
+
+import com.google.common.eventbus.EventBus
+import org.awaitility.kotlin.await
+import org.awaitility.kotlin.until
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import reactor.core.publisher.Flux
+import java.util.concurrent.atomic.AtomicReference
+
+internal class QuizProjectionForceEmitCommandTest {
+
+    @Test
+    fun shouldForceEmitOfQuiz() {
+        val quiz = Quiz(name = "Awesome Quiz")
+
+        val eventRepository = mock(EventRepository::class.java)
+        `when`(eventRepository.determineEvents(quiz.id))
+                .thenReturn(Flux.just(QuizCreatedEvent(quiz.id, quiz, 1)))
+
+        val eventBus = EventBus()
+        val quizProjection = QuizProjection(eventBus, mock(QuizStatisticsProvider::class.java), eventRepository)
+
+        val observedQuiz = AtomicReference<Quiz>()
+        quizProjection.observeQuiz(quiz.id)
+                .subscribe { observedQuiz.set(it) }
+
+        eventBus.post(ForceEmitCommand(quiz.id))
+
+        await until {
+            observedQuiz.get() == quiz
+        }
+    }
+
+}
