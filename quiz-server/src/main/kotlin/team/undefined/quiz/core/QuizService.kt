80db05a620bf677c4be3de1656e98b50a0b85332
André Schreck <andre.schreck@gmail.com>
2020-04-28 16:30:38 +0200

    46: added contracts

--- quiz-server/src/main/kotlin/team/undefined/quiz/core/QuizService.kt
+++ quiz-server/src/main/kotlin/team/undefined/quiz/core/QuizService.kt
@@ -1,58 +1,23 @@
 package team.undefined.quiz.core
 
-import com.google.common.eventbus.EventBus
-import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 
-@Service
-class QuizService(private val eventRepository: EventRepository,
-                  private val eventBus: EventBus) {
+interface QuizService {
 
-    fun createQuiz(command: CreateQuizCommand): Mono<Unit> {
-        return eventRepository.storeEvent(QuizCreatedEvent(command.quizId, command.quiz))
-                .map { eventBus.post(it) }
-    }
+    fun createQuiz(command: CreateQuizCommand): Mono<Unit>
 
-    fun createQuestion(command: CreateQuestionCommand): Mono<Unit> {
-        return eventRepository.storeEvent(QuestionCreatedEvent(command.quizId, command.question))
-                .map { eventBus.post(it) }
-    }
+    fun createQuestion(command: CreateQuestionCommand): Mono<Unit>
 
-    fun createParticipant(command: CreateParticipantCommand): Mono<Unit> {
-        eventBus.post(ForceEmitCommand(command.quizId))
-        return eventRepository.determineEvents(command.quizId)
-                .reduce(Quiz(name = "")) { quiz: Quiz, event: Event -> event.process(quiz)}
-                .filter { it.hasNoParticipantWithName(command.participant.name) }
-                .flatMap { eventRepository.storeEvent(ParticipantCreatedEvent(command.quizId, command.participant)) }
-                .map { eventBus.post(it) }
-    }
+    fun createParticipant(command: CreateParticipantCommand): Mono<Unit>
 
-    fun buzzer(command: BuzzerCommand): Mono<Unit> {
-        return eventRepository.determineEvents(command.quizId)
-                .reduce(Quiz(name = "")) { quiz: Quiz, event: Event -> event.process(quiz)}
-                .filter { it.nobodyHasBuzzered() }
-                .flatMap { eventRepository.storeEvent(BuzzeredEvent(command.quizId, command.participantId)) }
-                .map { eventBus.post(it) }
-    }
+    fun buzzer(command: BuzzerCommand): Mono<Unit>
 
-    fun startNewQuestion(command: AskQuestionCommand): Mono<Unit> {
-        return eventRepository.storeEvent(QuestionAskedEvent(command.quizId, command.questionId))
-                .map { eventBus.post(it) }
-    }
+    fun startNewQuestion(command: AskQuestionCommand): Mono<Unit>
 
-    fun answer(command: AnswerCommand): Mono<Unit> {
-        return eventRepository.storeEvent(AnsweredEvent(command.quizId, command.answer))
-                .map { eventBus.post(it) }
-    }
+    fun answer(command: AnswerCommand): Mono<Unit>
 
-    fun reopenQuestion(command: ReopenCurrentQuestionCommand): Mono<Unit> {
-        return eventRepository.storeEvent(CurrentQuestionReopenedEvent(command.quizId))
-                .map { eventBus.post(it) }
-    }
+    fun reopenQuestion(command: ReopenCurrentQuestionCommand): Mono<Unit>
 
-    fun finishQuiz(command: FinishQuizCommand): Mono<Unit> {
-        return eventRepository.storeEvent(QuizFinishedEvent(command.quizId))
-                .map { eventBus.post(it) }
-    }
+    fun finishQuiz(command: FinishQuizCommand): Mono<Unit>
 
 }
