84616d3552c6575b0c37e57de0cf88e17432bab0
André Schreck <gossie@users.noreply.github.com>
2020-05-01 14:19:05 +0200

    Button description

--- quiz-master-client/src/QuizMaster.tsx
+++ quiz-master-client/src/QuizMaster.tsx
@@ -47,6 +47,7 @@ const QuizMaster: React.FC<QuizMasterProps> = (props: QuizMasterProps) => {
                     <h4 className="title is-4">{quiz.name} (ID: {quiz.id})</h4>
                     <div className="columns">
                         <div className="column participants">
+                            When you push this button, the quiz will be closed and cannot be opened again. The final statisticts will be displayed.
                             <Participants quiz={quiz}></Participants>
                             { quiz.quizStatistics
                             ?
