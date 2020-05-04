68f8f898ad2fd3d364a0d21747cd77d1d15e5d49
André Schreck <andre.schreck@gmail.com>
2020-05-04 14:35:17 +0200

    #55: command line runner

--- quiz-server/src/main/kotlin/team/undefined/quiz/QuizApplication.kt
+++ quiz-server/src/main/kotlin/team/undefined/quiz/QuizApplication.kt
@@ -1,9 +1,11 @@
 package team.undefined.quiz
 
 import com.google.common.eventbus.EventBus
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import team.undefined.quiz.core.QuizService
 
 @SpringBootApplication
 class QuizApplication {
@@ -11,6 +13,11 @@ class QuizApplication {
 	@Bean
 	fun eventBus(): EventBus {
 		return EventBus()
+	}
+
+	@Bean
+	fun databaseCleaner(quizService: QuizService): CommandLineRunner {
+		return (CommandLineRunner { TODO("Not yet implemented") })
 	}
 
 }
