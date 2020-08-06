549dc324d2347047544647517c4a3c8c0bc022e1
André Schreck <andre.schreck@gmail.com>
2020-08-06 18:03:03 +0200

    css classes

--- quiz-master-client/src/Questions/Question/Question.tsx
+++ quiz-master-client/src/Questions/Question/Question.tsx
@@ -30,14 +30,18 @@ const QuestionElement: React.FC<QuestionElementProps> = (props: QuestionElementP
 
     return (
         <span>
-            <span data-testid="question">#{props.index + 1} {props.question.question}</span>
-            { props.question.estimates != null && <span data-testid={`freetext-question-${props.index}`}><i className="far fa-keyboard"></i></span> }
-            { props.question.estimates == null && <span data-testid={`buzzer-question-${props.index}`} className="icon has-text-danger"><i className="fas fa-circle"></i></span> }
-            { props.enableOperations && !props.question.pending && <span data-testid={`start-question-${props.index}`} className="icon has-text-primary" title="Ask question" onClick={() => toggleQuestion(props.question)}><i className="fas fa-share-square"></i></span>}
-            { props.enableOperations && !props.question.pending && props.onEdit && <span data-testid={`edit-question-${props.index}`} className="icon has-text-link" title="Edit question" onClick={() => props.onEdit!(props.question)}><i className="fas fa-edit"></i></span>}
-            { props.enableOperations && props.question.pending && <span data-testid={`revert-question-${props.index}`} className="icon has-text-primary"  onClick={() => toggleQuestion(props.question)}><i className="fas fa-undo"></i></span> }
-            { props.question.imagePath && props.question.imagePath.length > 0 && <span data-testid={`image-icon-${props.index}`} title="Show image" className="icon" onClick={() => props.setImageToDisplay(props.question.imagePath!)}><i className="fas fa-images"></i></span>}
-            { props.enableOperations && !props.question.pending && <span data-testid={`delete-question-${props.index}`} className="icon has-text-danger" title="Delete question" onClick={() => deleteQuestion(props.question)}><i className="fas fa-trash"></i></span>}
+            <div>
+                <span data-testid="question">#{props.index + 1} {props.question.question}</span>
+                { props.question.estimates != null && <span data-testid={`freetext-question-${props.index}`} className="icon"><i className="far fa-keyboard"></i></span> }
+                { props.question.estimates == null && <span data-testid={`buzzer-question-${props.index}`} className="icon has-text-danger"><i className="fas fa-circle"></i></span> }
+            </div>
+            <div>
+                { props.enableOperations && !props.question.pending && <span data-testid={`start-question-${props.index}`} className="icon has-text-primary" title="Ask question" onClick={() => toggleQuestion(props.question)}><i className="fas fa-share-square"></i></span>}
+                { props.enableOperations && !props.question.pending && props.onEdit && <span data-testid={`edit-question-${props.index}`} className="icon has-text-link" title="Edit question" onClick={() => props.onEdit!(props.question)}><i className="fas fa-edit"></i></span>}
+                { props.enableOperations && props.question.pending && <span data-testid={`revert-question-${props.index}`} className="icon has-text-primary"  onClick={() => toggleQuestion(props.question)}><i className="fas fa-undo"></i></span> }
+                { props.question.imagePath && props.question.imagePath.length > 0 && <span data-testid={`image-icon-${props.index}`} title="Show image" className="icon" onClick={() => props.setImageToDisplay(props.question.imagePath!)}><i className="fas fa-images"></i></span>}
+                { props.enableOperations && !props.question.pending && <span data-testid={`delete-question-${props.index}`} className="icon has-text-danger" title="Delete question" onClick={() => deleteQuestion(props.question)}><i className="fas fa-trash"></i></span>}
+            </div>
         </span>
     )
 };
