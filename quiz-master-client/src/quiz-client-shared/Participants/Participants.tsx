47d44139924f389ec8c891e8b8c0df0f7e932374
Sandra Siegel <11627788+sandrasie@users.noreply.github.com>
2020-10-04 17:24:43 +0200

    #87 Play buzzer sound in quiz master view if somebody has buzzered

--- quiz-master-client/src/quiz-client-shared/Participants/Participants.tsx
+++ quiz-master-client/src/quiz-client-shared/Participants/Participants.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import Quiz, { Participant } from "../quiz";
 import FlipMove from "react-flip-move"
 
 import './Participants.css';
 import ParticipantItem from './ParticipantItem';
+const buzzerfile = require('./../../assets/buzzer.mp3');
 
 interface ParticipantsProps {
     quiz: Quiz;
@@ -15,11 +16,13 @@ interface ParticipantState {
 }
 
 const Participants: React.FC<ParticipantsProps> = (props: ParticipantsProps) => {
-
+    const buzzerAudio = useRef(null);
     const pendingQuestion = props.quiz.openQuestions.find(question => question.pending);
+    const itsAPlayersTurn = props.quiz.participants.some(p => p.turn);
     const [stateAfterLastQuestion, setStateAfterLastQuestion] = useState(new Array<ParticipantState>()); 
-    const [currentQuestionId, setCurrentQuestionId] = useState('');
- 
+    const [currentQuestionId, setCurrentQuestionId] = useState("none");
+    const [wasAPlayersTurnBefore, setWasAPlayersTurnBefore] = useState(false);
+
     const getPointsAfterLastQuestionForParticipant = (participant: Participant) => {
         const participantStateAfterLastQuestion = stateAfterLastQuestion.find(p => p.id === participant.id);
         if (participantStateAfterLastQuestion) {
@@ -29,8 +32,9 @@ const Participants: React.FC<ParticipantsProps> = (props: ParticipantsProps) => 
             return participantCurrentState ? participantCurrentState.points : 0;
         }
     }
+    
     const updateStateAfterLastQuestion = useCallback(() => {
-        setCurrentQuestionId(pendingQuestion ? pendingQuestion.id : '');
+        setCurrentQuestionId(pendingQuestion ? pendingQuestion.id : "none");
         setStateAfterLastQuestion(props.quiz.participants.map(p => { return {id: p.id, points: p.points}}));
     }, [props.quiz.participants, pendingQuestion]);
 
@@ -38,12 +42,30 @@ const Participants: React.FC<ParticipantsProps> = (props: ParticipantsProps) => 
         return pendingQuestion && currentQuestionId !== pendingQuestion.id;
     }, [pendingQuestion, currentQuestionId]); 
 
+    const playerHasBuzzered = useCallback(() => {
+        return !wasAPlayersTurnBefore && itsAPlayersTurn; 
+    }, [wasAPlayersTurnBefore, itsAPlayersTurn]);
+
+    useEffect(() => {
+        if (playerHasBuzzered()) {
+            buzzerAudio.current.muted = false;
+            buzzerAudio.current.play();
+        }
+        setWasAPlayersTurnBefore(itsAPlayersTurn)
+    }, [playerHasBuzzered, itsAPlayersTurn, setWasAPlayersTurnBefore]);
+
     useEffect(() => {
         if (isNewQuestion()) {
             updateStateAfterLastQuestion();
         } 
     }, [isNewQuestion, updateStateAfterLastQuestion]);
     
+    useEffect(() => {
+        // Trigger preloading of audio to prevent delays when buzzer is pressed
+        buzzerAudio.current.muted = true;
+        buzzerAudio.current.play();
+    }, [])
+
     const comparePoints = (a: Participant, b: Participant) => {
         if (b.points === a.points) {
             return a.name.localeCompare(b.name);
@@ -52,24 +74,21 @@ const Participants: React.FC<ParticipantsProps> = (props: ParticipantsProps) => 
         }  
     }
 
-    const elements = props.quiz.participants
-        ?.sort(comparePoints)
-        .map((p, index) => 
-            <div key={p.name}>
-                <ParticipantItem quiz={props.quiz} participant={p} pointsAfterLastQuestion={getPointsAfterLastQuestionForParticipant(p)}>
-                </ParticipantItem>
-            </div>
-        )
+    const elements = props.quiz.participants?.sort(comparePoints).map(p => 
+        <div key={p.name}>
+            <ParticipantItem quiz={props.quiz} participant={p} pointsAfterLastQuestion={getPointsAfterLastQuestionForParticipant(p)}>
+            </ParticipantItem>
+        </div>)
 
     return (
         <div>
-            <h4 className="title is-4">Participants</h4>
+            <audio src={buzzerfile} ref={buzzerAudio} preload='auto'></audio>
+            <h5 className="title is-5">Participants</h5>
             <div data-testid="participants" className="participants-list">
                 <FlipMove>
                     {elements}
                 </FlipMove>     
             </div>
-            { pendingQuestion && pendingQuestion.secondsLeft && <span data-testid="question-counter">{pendingQuestion.secondsLeft} seconds left</span> }
         </div>
     )
 };
