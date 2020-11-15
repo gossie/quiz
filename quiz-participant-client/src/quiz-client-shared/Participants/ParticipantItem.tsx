a47df7df48e89f89ab6deee3248ca4ce10f1b070
Sandra Siegel <11627788+sandrasie@users.noreply.github.com>
2020-11-15 20:43:28 +0100

    Apply redesign also to participant app

--- quiz-participant-client/src/quiz-client-shared/Participants/ParticipantItem.tsx
+++ quiz-participant-client/src/quiz-client-shared/Participants/ParticipantItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Participant, Question } from "../quiz";
+import './ParticipantItem.scss';
 
 interface ParticipantProps {
     participant: Participant;
@@ -15,11 +16,31 @@ const ParticipantItem: React.FC<ParticipantProps> = (props: ParticipantProps) =>
         }
     }
 
-    return <div data-testid="participant-wrapper" className={"participant " + (props.participant.turn ? 'turn' : '')}>
-                <span data-testid="participant-name">{props.participant.name} </span>
-                <div className="points">({props.pointsAfterLastQuestion}{pointDifference()})</div>
-                { props.question && !props.question.revealed && props.question.estimates && props.question.estimates[props.participant.id] && <span data-testid="answer-hint" className="icon"><i className="far fa-comment"></i></span> }
-                { props.question && props.question.revealed && props.question.estimates && props.question.estimates[props.participant.id] && <span>{props.question.estimates[props.participant.id]}</span> }
+    const getEstimatedValue = () => {
+        if (props.question) {
+            if (props.question.revealed) {
+                return props.question.estimates != null && props.question.estimates[props.participant.id] != null
+                ? props.question.estimates[props.participant.id]
+                : '';
+            } else {
+                return props.question.estimates != null && props.question.estimates[props.participant.id] != null
+                ? '*****'
+                : '';
+            }
+        }
+        return null;
+    }
+
+    return <div data-testid="participant-wrapper" className="participant">
+                <div className={"participant-header"}>             
+                    <span data-testid="participant-name" className="participant-name">{props.participant.name} </span>
+                    <div className="points">({props.pointsAfterLastQuestion}{pointDifference()})</div>
+                </div>
+                <div className={'participant-answer' + (props.participant.turn || getEstimatedValue() ? ' visible': '')}>
+                    <div className="bubble">
+                        {props.participant.turn ? 'I have buzzered!' : getEstimatedValue()}
+                    </div>
+                </div>
             </div>;
 
 }
