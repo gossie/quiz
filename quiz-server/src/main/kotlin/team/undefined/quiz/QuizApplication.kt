aa216f1a84610b420e271ae04f257ffffbddae4e
André Schreck <andre.schreck@gmail.com>
2020-11-22 00:04:40 +0100

    delete old quizzes twice a day

--- quiz-server/src/main/kotlin/team/undefined/quiz/QuizApplication.kt
+++ quiz-server/src/main/kotlin/team/undefined/quiz/QuizApplication.kt
@@ -6,15 +6,18 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import reactor.core.publisher.Flux
 import team.undefined.quiz.core.DeleteQuizCommand
 import team.undefined.quiz.core.QuizService
+import java.time.Duration
+import java.time.Period
 import java.util.*
 
 @SpringBootApplication
 class QuizApplication {
 
     private val logger = LoggerFactory.getLogger(QuizApplication::class.java)
-    private val ACTIVE_TIME = 6_048_000_000
+    private val ACTIVE_TIME = 2_419_200_000
 
     @Bean
     fun eventBus(): EventBus {
@@ -24,15 +27,11 @@ class QuizApplication {
     @Bean
     fun databaseCleaner(quizService: QuizService): CommandLineRunner {
         return (CommandLineRunner {
-            quizService.determineQuizzes()
+            Flux.interval(Duration.ofSeconds(30), Duration.ofHours(12))
+                    .flatMap { quizService.determineQuizzes() }
                     .filter { it.getTimestamp() < (Date().time - ACTIVE_TIME) }
-                    .flatMap {
-                        logger.info("deleting quiz {} because it is form {}", it, Date(it.getTimestamp()))
-                        quizService.deleteQuiz(DeleteQuizCommand(it.id))
-                    }
-                    .subscribe {
-                        logger.info("deleted old quiz")
-                    }
+                    .flatMap { quizService.deleteQuiz(DeleteQuizCommand(it.id)) }
+                    .subscribe { logger.info("deleted old quiz") }
         })
     }
 
