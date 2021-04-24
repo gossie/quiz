e4bb19ce138182bbcadab882b78338b4bdf6f654
André Schreck <andre.schreck@gmail.com>
2021-04-24 18:50:06 +0200

    removed import

--- quiz-master-client/src/quiz-client-shared/QuizStatistics/QuizStatistics.tsx
+++ quiz-master-client/src/quiz-client-shared/QuizStatistics/QuizStatistics.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Quiz from "../quiz";
 import './QuizStatistics.scss';
