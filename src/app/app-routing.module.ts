cd6b8d2054e9a601f691970325db2ef7094df8bf
André Schreck <andre.schreck@gmail.com>
2021-12-30 13:35:44 +0100

    path parameters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+    { path: '', redirectTo: 'quiz-host', pathMatch: 'full' },
     { path: 'quiz-host', loadChildren: () => import('./quiz-host/quiz-host.module').then(m => m.QuizHostModule) },
-    { path: 'quiz-participant', loadChildren: () => import('./quiz-participant/quiz-participant.module').then(m => m.QuizParticipantModule) }
+    { path: 'quiz-host/:quizId', loadChildren: () => import('./quiz-host/quiz-host.module').then(m => m.QuizHostModule) },
+    { path: 'quiz-participant', loadChildren: () => import('./quiz-participant/quiz-participant.module').then(m => m.QuizParticipantModule) },
+    { path: 'quiz-participant/:quizId', loadChildren: () => import('./quiz-participant/quiz-participant.module').then(m => m.QuizParticipantModule) }
 ];
 
 @NgModule({
