713d9867e8e952dadc288957b83d00e41acb26d9
André Schreck <andre.schreck@gmail.com>
2021-12-30 14:52:36 +0100

    amplify api

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "quiz": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
